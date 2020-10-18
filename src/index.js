ecd6cfe25aee317fca302945d77a8c899e6817b7
Andrey <opexsomoff@gmail.com>
2020-10-18 18:27:31 +0900

    feat: decided

--- src/index.js
+++ src/index.js
@@ -38,7 +38,40 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const obj = {};
+    const arrExpr = [];
+    const result = [];
+    const keys = Object.keys(MORSE_TABLE);
+
+    keys.forEach((key) => {
+        let acc = '';
+        let temp = '';
+        for (let i = 0; i < key.length; i += 1) {
+            acc += key[i] === '.' ? '10' : '11'
+        }
+        if (acc.length < 10) {
+            const counter = 10 - acc.length;
+            for (let i = 0; i < counter; i += 1) {
+                temp += 0;
+            }
+            temp += acc;
+        };
+        !temp ? obj[acc] = MORSE_TABLE[key] : obj[temp] = MORSE_TABLE[key];
+    });
+
+    
+    for (let i = 0; i < expr.length; i += 10) {
+        arrExpr.push(expr.substring(i, i + 10))
+    }
+
+    arrExpr.forEach((tenElements) => {
+        if (tenElements === `**********`) result.push(' ');
+        else {
+            result.push(obj[tenElements])
+        }
+    })
+    
+    return result.join('');
 }
 
 module.exports = {
